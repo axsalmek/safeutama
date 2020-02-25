7705fd3b81910c4ff5d01828c01f2ea9a2e98e6d
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-02-26 00:18:08 +0700

    Update post human5.js

--- post human5.js
+++ post human5.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.aksurance.me/2019/12/how-to-choose-sports-that-are-good-and.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://kyasafel.blogspot.com/2019/12/how-to-choose-sports-that-are-good-and.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
