e83945b0117c0c51907ddfa881c79b10d9a53045
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-02-26 00:18:38 +0700

    Update post human3.js

--- post human3.js
+++ post human3.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.aksurance.me/2019/12/7-makanan-terbaik-untuk-paru-paru-yang.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://kyasafel.blogspot.com/2019/12/7-makanan-terbaik-untuk-paru-paru-yang.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
