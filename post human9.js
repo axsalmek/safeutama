4323cc8f61a8d4f3ff91c7745d8d523e3ce86cc1
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2021-11-21 05:36:02 +0700

    Update post human9.js

--- post human9.js
+++ post human9.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://trendingworld.xyz/your-underarms-smell-so-bad/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://trendingworld.xyz/actor-mark-york-billy-merchant-on-the-office-dead-at-55-years-old/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
