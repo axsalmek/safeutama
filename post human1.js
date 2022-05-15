b1b5e0b030cb1b486b0457db51475f2ef980232b
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2022-05-15 14:23:43 +0700

    Update post human1.js

--- post human1.js
+++ post human1.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.trendingworld.xyz/2021/10/your-underarms-smell-so-bad.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://trandingworldnow.blogspot.com/2021/10/your-underarms-smell-so-bad.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
